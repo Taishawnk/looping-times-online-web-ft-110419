8c21b1780d982941a6b6cc66f504ab3197b555c0
Taishawn King <taishawnking1@yahoo.com>
2019-11-06 01:32:33 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,6 @@
-def using_times(looping_string = "Wingardium Leviosa")
-
-7.times puts looping_string
-
+def using_times
+  looping_string = "Wingardium Leviosa"
+  7.times puts looping_string
+  end
 end
-
-
+using_times
